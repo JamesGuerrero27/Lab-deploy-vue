80ff56873eb7c0c53ae5083bb5a180e114b13fde
JamesGuerrero27 <jamessteve_27@hotmail.com>
2019-06-18 06:35:12 -0600

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+<form class="w-50 text-center moveCenterForm">
+  <div class="form-group text-center inline-block mr">
+    <label for="exampleFormControlInput1">Correo Electronico</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+   <div class="form-group">
+    <label for="exampleFormControlInput1">Titulo de mensaje</label>
+    <input type="text" class="form-control" placeholder="Titulo">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Contenido</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Contenido"></textarea>
+  </div>
+  <div class="form-group">
+    <button type="button" class="btn btn-primary">Enviar</button>
+  </div>
+</form>
+</template>
+
+<style scoped>
+.moveCenterForm{
+  margin-left: 25%;
+}
+</style>
